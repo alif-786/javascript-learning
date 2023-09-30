80d3b3bab47c7d1503c357f1de1107f964d9fe20
alif-786 <alipkamate1@gmail.com>
2023-09-30 19:39:04 +0530

    date time complete

--- date_time.js
+++ date_time.js
@@ -10,3 +10,21 @@ let date = new Date();
 // let myCreatedDate = new Date("01-24-2004");
 let myCreatedDate = new Date(2004, 1, 24, 8, 15);
 console.log(myCreatedDate.toLocaleString())
+
+
+// create a new date and also console it
+
+let myTimeStamp = Date.now();
+
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime() / 1000)
+
+// creativity with the localstring 
+console.log(date.toLocaleString('default', {
+    weekday: 'long',
+    timeZone: 'Asia/Kolkata',
+    hour: "2-digit",
+    minute: "2-digit",
+}))
+
+
