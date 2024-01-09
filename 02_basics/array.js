693c8c25a3546c4da1374fdd8e363c6582250096
alif-786 <alipkamate1@gmail.com>
2024-01-09 11:54:53 +0530

    created arrays and printed them

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -1,2 +1,7 @@
+// array
 
-console.log("hello array")
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ['captain america', 'iron man ', 'deadpool', 'thor', 'hawkeye', 'quick silver', 'hulk']
+
+console.log(myArr[0])
+console.log(myHeros[1])
